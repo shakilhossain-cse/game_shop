1a34ad5822a025c600c9ae0443d281077628fe22
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-09-26 16:08:54 +0600

    front awosom and react rating packge install and header component done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return ( 
+      <header className="bg-light container my-5 py-4">
+        <h1 className="text-center">Total Amount $500 For Buy Games</h1>
+      </header>
+  );
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                    
+                </div>
+                <div className="col-md-4">
+
+                </div>
+            </div>
+        </div>
+    )
+}
