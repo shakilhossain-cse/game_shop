e006787719e9bb647a85cc4ff067086688609473
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-09-26 22:24:54 +0600

    display cart total amount

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 export default function Cart(props) {
-  console.log(props.carts);
+  const {image,name,price}=props.cart;
   return (
-    <div>
-      <div className="card p-2">
-        <h2>Total Added {props.carts.length}</h2>
+    <div className="my-2 bg-light">
+      <div className="d-flex align-items-center justify-content-center">
+          <div className="w-25">
+            <img src={image} className="w-50 h-50" alt="" />
+          </div>
+          <div className="w-75">
+            <h5>{name}</h5>
+            <p>price : ${price}</p>
+          </div>
       </div>
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,34 +3,57 @@ import Cart from "../Cart/Cart";
 import Game from "../Game/Game";
 
 export default function Main() {
+  const [games, setGames] = useState([]);
+  const [carts, setCarts] = useState([]);
+  const [cartotal , setCartotal] = useState(0)
 
-    const [games, setGames] = useState([])
-    const [carts , setCarts] = useState([])
+  const handelClick = (cart) => {
+    const newCart = [...carts, cart];
+    setCarts(newCart);
+    let amount = 0;
+    for (const cart of newCart) {
+        amount =parseFloat(cart.price) + amount
+    }
+    setCartotal(amount)
+  };
+  // const [newcalculation, setNewcalculation] = useState(0);
 
-    const handelClick = cart => {
-      const newCart = [...carts,cart];
-      setCarts(newCart);
-    } 
+  // for (const cart of carts) {
+  //   const newcal = cart.price + newcalculation
+  //   setNewcalculation(newcal)
+  // }
 
-    useEffect(() => {
-        fetch('./game.json')
-        .then(res => res.json())
-        .then(data => setGames(data))
-    }, [])
+  // console.log(calculation);
+  useEffect(() => {
+    fetch("./game.json")
+      .then((res) => res.json())
+      .then((data) => setGames(data));
+  }, []);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-8 bg-light">
-        <div className="row row-cols-1 row-cols-md-3 g-4 py-3">
-            {
-                games.map(game =>  <Game game={game} key={game.id} handelClick={handelClick}/>)
-            }
-        </div>
+          <div className="row row-cols-1 row-cols-md-3 g-4 py-3">
+            {games.map((game) => (
+              <Game game={game} key={game.id} handelClick={handelClick} />
+            ))}
+          </div>
         </div>
         <div className="col-md-4 bg-light">
           <div className="py-3">
-            <Cart carts={carts}/>
+            <div className="card p-2">
+              <h2>Total Added {carts.length}</h2>
+              {carts.map((cart,index) => (
+                <Cart cart={cart} key={index} />
+              ))}
+              <div className=" d-flex justify-content-between border-top p-3">
+                <h5>Total Price </h5>
+                <h5>
+                  {cartotal}
+                </h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
